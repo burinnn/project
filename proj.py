ce4ba13b69a3492b9ee74fe51ef200d614371634
burinnn <100030471+burinnn@users.noreply.github.com>
2022-04-21 09:12:43 +0300

    Create proj.py

---
+++ proj.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+
+
+def credit():
+  result = int(e1.get()) * int(e2.get())e3.get()
+  e4.clipboard_clear()
+  e4.insert(0, result)
+
+
+root = Tk()
+l1 = Label(text="Сумма кредита")
+e1 = Entry(width=50)
+l2 = Label(text="Срок")
+e2 = Entry(width=50)
+l3 = Label(text="Первоначальный взнос")
+e3 = Entry(width=50)
+but = Button(text="Рассчитать кредит",command=credit)
+l4 = Label(text="ИТОГ")
+e4 = Entry(width=50)
+l1.pack()
+e1.pack()
+l2.pack()
+e2.pack()
+l3.pack()
+e3.pack()
+but.pack()
+l4.pack()
+e4.pack()
+
+
+root.mainloop()
